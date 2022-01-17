b7e5be24b471e94ae1b7ee3dab2e3fa5dcb94d42
YeonJun Jo <micayel1219@gmail.com>
2022-01-17 17:57:28 +0900

    2nd Commit : MySQL, MyBatis connect

---
+++ demo/src/main/java/com/example/demo/mapper/UserProfileMapper.java
@@ -0,0 +1,35 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.demo.model.UserProfile;
+
+@Mapper
+public interface UserProfileMapper {
+
+	@Select("SELECT * FROM UserProfile WHERE id=#{id}")
+	UserProfile getUserProfile(@Param("id")String id);
+	
+	@Select("SELECT * FROM UserProfile")
+	List<UserProfile> getUserProfileList();
+	
+	
+	//영향 받은 레코드 수 반환
+	@Insert("INSERT INTO UserProfile VALUES(#{id}, #{name}, #{phone}, #{address})")
+	int insertUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address );
+	
+	//영향 받은 레코드 수 반환
+	@Update("UPDATE UserProfile SET name=#{name}, phone=#{phone}, address=#{address} WHERE id=#{id}")
+	int updateUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address );
+	
+	//영향 받은 레코드 수 반환
+	@Delete("DELETE FROM UserProfile WHERE id=#{id}")
+	int deleteUserProfile(@Param("id") String id);
+}
